5780f599e68e4cb484e4605056f80c3d72c9bce8
Daniel Savidge <55741686+DanielSavidge@users.noreply.github.com>
2020-11-09 19:05:13 +0000

    Update factory.go

--- factory/factory.go
+++ factory/factory.go
@@ -2,19 +2,14 @@ package main
 
 import ("flag"
 	"fmt"
-	//"net"
+	"net"
 	"pairbroker/stubs"
 	"net/rpc")
 
 
 type Factory struct {}
 
-//TODO: Define a Multiply function to be accessed via RPC.
-func (f *Factory) Multiply(pair stubs.Pair, res stubs.JobReport) (err error){
-	 res.Result = (pair.X) *(pair.Y)
-	 fmt.Println("Pair multiplication")
-	 return
-}
+//TODO: Define a Multiply function to be accessed via RPC. 
 //Check the previous weeks' examples to figure out how to do this.
 
 
@@ -22,15 +17,5 @@ func main(){
 	pAddr := flag.String("ip", "127.0.0.1:8050", "IP and port to listen on")
 	brokerAddr := flag.String("broker","127.0.0.1:8030", "Address of broker instance")
 	flag.Parse()
-
-
-	client,_ := rpc.Dial("tcp",*brokerAddr)
-	defer client.Close()
-
-	subscriptionx := stubs.Subscription{Topic:"multiply",FactoryAddress:*pAddr,Callback:*brokerAddr}
-	response:=stubs.JobReport{}
-	client.Call(stubs.Subscribe,subscriptionx,response)
-	//fmt.Println("Responded: "+response.Message)
-
 	//TODO: You'll need to set up the RPC server, and subscribe to the running broker instance.
 }
